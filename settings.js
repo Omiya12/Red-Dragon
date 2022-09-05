68bd11b0e1422cc19ba49dc5bb23bc28d317fd75
Omiya12 <112845354+Omiya12@users.noreply.github.com>
2022-09-05 11:30:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,10 +29,10 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
+global.vcardowner = ['+94760412914'] // owner number
 global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
 global.ytname = "" // yt chanel name
-global.socialm = "GitHub: prabhasha2006" // github or insta name
+global.socialm = "https://youtu.be/aaXx_wPavpY" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
 
 //bot body 
@@ -47,13 +47,13 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
-global.packname = "Bot Sticker" // sticker packname
+global.owner = ['+94760412914']
+global.ownertag = '+94760412914' //your tag number
+global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 ꧁༒︎𝙾𝙼𝙸𝚈𝙰༒︎꧂' //ur bot name
+global.packname = "꧁༒︎𝙾𝙼𝙸𝚈𝙰༒︎꧂" // sticker packname
 global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
+global.footer = '[𝙸 𝚊𝚖 𝚊𝚕𝚒𝚟𝚎 𝚗𝚘𝚠 https://youtu.be/aaXx_wPavpY]' //
 
 //database
 global.premium = ['+380944814219'] //ur premium numbers
